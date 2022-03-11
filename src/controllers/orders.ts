67a0c688fb6431c8114f4e88b3eee1d90e62096e
dougllasfm <douglasmunaro@gmail.com>
2022-03-11 15:12:59 -0300

    rota de ler pedidos feitos com a empresa

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Products, ProductsOnOrders } from "@prisma/client";
 import { Response, Request } from "express";
 const prisma = new PrismaClient();
 
@@ -48,6 +48,13 @@ const listOrdersUser = async function (req: Request, res: Response) {
   }
 };
 
+type OrderProps = {
+  orderId: number;
+  price: string;
+  quantity: number;
+  products: string[];
+};
+
 const listOrders = async function (req: Request, res: Response) {
   try {
     const listOrders = await prisma.orders.findMany({
@@ -59,7 +66,50 @@ const listOrders = async function (req: Request, res: Response) {
         },
       },
     });
-    res.status(200).json(listOrders);
+    let arrayOrders: OrderProps[] = [];
+    let flag = 0;
+
+    listOrders.map((item) => {
+      let productsArray: string[] = [];
+      flag = 0;
+      item.products.map((element) => {
+        if (arrayOrders.length === 0) {
+          productsArray.push(element.product.name);
+          arrayOrders.push({
+            orderId: element.orderId,
+            price: element.product.price,
+            quantity: 1,
+            products: [...productsArray],
+          });
+        } else {
+          arrayOrders.forEach((product) => {
+            if (product.orderId === element.orderId) {
+              productsArray.push(element.product.name);
+              product.price = (
+                parseInt(product.price) + parseInt(element.product.price)
+              )
+                .toFixed(2)
+                .toString()
+                .replaceAll(".", ",");
+              product.quantity = product.quantity + 1;
+              product.products = [...productsArray];
+              return { product };
+            } else if (flag == 0) {
+              flag = 1;
+              productsArray.push(element.product.name);
+              arrayOrders.push({
+                orderId: element.orderId,
+                price: element.product.price,
+                quantity: 1,
+                products: [...productsArray],
+              });
+            }
+          });
+        }
+      });
+    });
+
+    res.status(200).json(arrayOrders);
   } catch (error) {
     res.status(400).send({ error });
     console.log(error);
